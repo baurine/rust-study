7193a019c31ff54a9c7e3442ce0b008855ca4e64
baurine <2008.hbl@gmail.com>
2019-12-05 22:36:38 +0800

    euler 7

---
+++ codes/euler/src/euler_7.rs
@@ -0,0 +1,38 @@
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+fn is_prime(n: u32) -> bool {
+  for i in 2..(n / 2 + 1) {
+    if n % i == 0 {
+      return false;
+    }
+  }
+  true
+}
+
+pub fn solution() -> u32 {
+  let mut order = 1;
+  for i in (3..).step_by(2) {
+    if is_prime(i) {
+      order += 1;
+      println!("{}st: {}", order, i);
+      if order == 10_001 {
+        return i;
+      }
+    }
+  }
+  0
+}
+
+/////////////////////////////////////
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_solution_1() {
+    assert_eq!(solution(), 104743);
+  }
+}
--- codes/euler/src/main.rs
+++ codes/euler/src/main.rs
@@ -4,6 +4,7 @@ mod euler_3;
 mod euler_4;
 mod euler_5;
 mod euler_6;
+mod euler_7;
 
 fn main() {
   println!("euler project!");
