f9b9c5dd689a1202640a674565af527aec2dac34
baurine <2008.hbl@gmail.com>
2019-12-05 22:44:35 +0800

    euler 1

---
+++ codes/euler/src/euler_1.rs
@@ -0,0 +1,32 @@
+/*
+求小于 1000 的能被 3 或 5 整除的所有整数之和。
+*/
+pub fn solution_1() -> u32 {
+  let mut sum = 0u32;
+  for i in 1..1000 {
+    if i % 3 == 0 || i % 5 == 0 {
+      sum += i;
+    }
+  }
+  sum
+}
+
+pub fn solution_2() -> u32 {
+  (1..1000).filter(|x| x % 3 == 0 || x % 5 == 0).sum::<u32>()
+}
+
+/////////////////////////////////////
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_solution_1() {
+    assert_eq!(solution_1(), 233168);
+  }
+
+  #[test]
+  fn test_solution_2() {
+    assert_eq!(solution_2(), 233168);
+  }
+}
---
+++ codes/euler/src/main.rs
@@ -0,0 +1,6 @@
+mod euler_1;
+
+fn main() {
+    println!("{}", euler_1::solution_1());
+    println!("{}", euler_1::solution_2());
+}
