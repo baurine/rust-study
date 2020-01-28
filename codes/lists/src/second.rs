6d94cf62792a1ddb29e26a299243c994e2274fb0
baurine <2008.hbl@gmail.com>
2020-01-28 22:47:18 +0800

    linked-list second - Peek / Len / IntoIter

--- codes/lists/src/second.rs
+++ codes/lists/src/second.rs
@@ -52,6 +52,41 @@ impl<T> List<T> {
       node.elem
     })
   }
+
+  pub fn peek(&self) -> Option<&T> {
+    self.head.as_ref().map(|node| &node.elem)
+  }
+
+  pub fn peek_mut(&mut self) -> Option<&mut T> {
+    self.head.as_mut().map(|node| &mut node.elem)
+  }
+
+  pub fn len(&self) -> usize {
+    let mut len = 0;
+    let mut cur_link = self.head.as_ref();
+    while let Some(boxed_node) = cur_link {
+      cur_link = boxed_node.next.as_ref();
+      len += 1
+    }
+    len
+  }
+}
+
+///////////////
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+  pub fn into_iter(self) -> IntoIter<T> {
+    IntoIter(self)
+  }
+}
+
+impl<T> Iterator for IntoIter<T> {
+  type Item = T;
+  fn next(&mut self) -> Option<Self::Item> {
+    self.0.pop()
+  }
 }
 
 impl<T> Drop for List<T> {
@@ -99,5 +134,49 @@ mod test {
     // Check exhaustion
     assert_eq!(list.pop(), Some(1));
     assert_eq!(list.pop(), None);
+  }
+
+  #[test]
+  fn peek() {
+    let mut list = List::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
+    // list.peek_mut().map(|&mut value| value = 42);
+    list.peek_mut().map(|value| *value = 42);
+
+    assert_eq!(list.peek(), Some(&42));
+    assert_eq!(list.pop(), Some(42));
+  }
+
+  #[test]
+  fn len() {
+    let mut list = List::new();
+    assert_eq!(list.len(), 0);
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    assert_eq!(list.len(), 3);
+    list.pop();
+    assert_eq!(list.len(), 2);
+  }
+
+  #[test]
+  fn into_iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
   }
 }
